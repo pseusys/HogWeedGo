b1b60b6cc5c28fc0cb57fe5fc8c2d61d38adf4c3
milesartemius <shveitsar215@gmail.com>
2021-10-06 07:02:10 +0300

    reload location fixed

--- client/lib/map_page.dart
+++ client/lib/map_page.dart
@@ -19,49 +19,17 @@ class MapPage extends StatefulWidget {
 class _MapPageState extends State<MapPage> {
   LatLng? _me;
 
-  void _check_permissions() {
-    WidgetsBinding.instance?.addPostFrameCallback((_) async {
-      bool serviceEnabled;
-      LocationPermission permission;
-
-      serviceEnabled = await Geolocator.isLocationServiceEnabled();
-      if (!serviceEnabled) {
-        // Location services are not enabled don't continue accessing the position and request users of the App to enable the location services.
-        return Future.error('Location services are disabled.');
-      }
-
-      permission = await Geolocator.checkPermission();
-      if (permission == LocationPermission.denied) {
-        permission = await Geolocator.requestPermission();
-        if (permission == LocationPermission.denied) {
-          // Permissions are denied, next time you could try requesting permissions again (this is also where Android's shouldShowRequestPermissionRationale returned true. According to Android guidelines your App should show an explanatory UI now.
-          return Future.error('Location permissions are denied');
-        }
-      }
-
-      if (permission == LocationPermission.deniedForever) {
-        // Permissions are denied forever, handle appropriately.
-        return Future.error('Location permissions are permanently denied, we cannot request permissions.');
-      }
-
-      // When we reach here, permissions are granted and we can continue accessing the position of the device.
-    });
-  }
+  void _setMe(Position? p) => setState(() { _me = p != null ? LatLng(p.latitude, p.longitude) : null; });
 
   @override
   void initState() {
     super.initState();
-    Geolocator.getPositionStream().listen((Position? position) {
-      print(position);
-      if (position != null) {
-        setState(() => _me = LatLng(position.latitude, position.longitude));
-      }
-    });
+    Geolocator.getCurrentPosition().then((value) => _setMe(value)).catchError((e) => print("No location provided!"));
+    Geolocator.getPositionStream().listen((Position? p) => _setMe(p)).onError((e) => print("No location provided!"));
   }
 
   @override
   Widget build(BuildContext context) {
-    print(_me);
     return Scaffold(
         appBar: AppBar(title: Text(widget.title)),
 
