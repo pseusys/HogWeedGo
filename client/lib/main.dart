9b294bda90e6bf435ac51d1a07f38058d3d1a2f0
milesartemius <shveitsar215@gmail.com>
2021-10-17 15:51:29 +0300

    build script fixed

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -57,7 +57,7 @@ class HogWeedGo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'HogWeedGo',
       initialRoute: MapPage.route,
       theme: ThemeData(),
 
