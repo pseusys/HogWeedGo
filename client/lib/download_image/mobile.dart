c786413d8255e5c130d46676619d195c01ddd362
milesartemius <shveitsar215@gmail.com>
2021-11-10 07:47:09 +0300

    formal branch added

--- client/lib/download_image/mobile.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_cache_manager/flutter_cache_manager.dart';
-import 'package:gallery_saver/gallery_saver.dart';
-
-
-Future<String?> saveFileFromUri(String name, String uri) async {
-  final image = await DefaultCacheManager().getSingleFile(uri);
-  await GallerySaver.saveImage(image.path);
-}
--- client/lib/misc/extensions.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-
-extension NavigatorStateExtension on NavigatorState {
-  void popAllAndPushNamed(String routeName, {Object? arguments}) {
-    popUntil(ModalRoute.withName(Navigator.defaultRouteName));
-    pushNamed(routeName, arguments: arguments);
-  }
-}
