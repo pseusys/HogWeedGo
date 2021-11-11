23f1ba568622e6ea4fea18c30e43b8e32ad9e885
milesartemius <shveitsar215@gmail.com>
2021-11-11 18:35:40 +0300

    removed unnecessary scrolling assisting bar

--- client/lib/views/report_view.dart
+++ client/lib/views/report_view.dart
@@ -34,98 +34,81 @@ class ReportView extends StatelessWidget {
     return DraggableScrollableSheet(
       expand: false,
       builder: (_, ScrollController scrollController) {
-        return Column(
+        return ListView(
+          padding: const EdgeInsets.symmetric(horizontal: OFFSET),
+          controller: scrollController,
           children: [
-            MouseRegion(
-              cursor: SystemMouseCursors.click,
-              child: Container(
-                width: OFFSET * 2,
-                height: GAP,
-                margin: const EdgeInsets.symmetric(vertical: GAP),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(MARGIN),
-                  color: Colors.grey,
+            const SizedBox(height: MARGIN),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text('type', style: Theme.of(context).textTheme.headline4),
+                const Chip(
+                  avatar: CircleAvatar(
+                    child: Text('ST'),
+                  ),
+                  label: Text('status'),
                 ),
-              ),
+              ],
             ),
+            const SizedBox(height: MARGIN),
 
-            Expanded(
-              child: ListView(
-                padding: const EdgeInsets.symmetric(horizontal: OFFSET),
-                controller: scrollController,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Text('type', style: Theme.of(context).textTheme.headline4),
-                      const Chip(
-                        avatar: CircleAvatar(
-                          child: Text('ST'),
-                        ),
-                        label: Text('status'),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: MARGIN),
-
-                  Row(
-                    children: [
-                      const Icon(Icons.location_on),
-                      const SizedBox(width: MARGIN),
-                      Column(
-                        children: [
-                          Text('address', style: Theme.of(context).textTheme.headline6),
-                          const Text('coords'),
-                        ],
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: MARGIN),
-
-                  Row(
-                    children: [
-                      const Icon(Icons.access_time),
-                      const SizedBox(width: MARGIN),
-                      Text('time', style: Theme.of(context).textTheme.headline6),
-                    ],
-                  ),
-                  const SizedBox(height: MARGIN),
-
-                  const PhotoGallery(false),
-                  const SizedBox(height: MARGIN),
-
-                  Row(
-                    children: const [
-                      Icon(Icons.description),
-                      SizedBox(width: MARGIN),
-                      Text('description'),
-                    ],
-                  ),
-                  const SizedBox(height: MARGIN),
-
-                  _comment("initial comment", context, username: "report sender"),
-
-                  const Divider(
-                    thickness: 5,
-                    indent: 20,
-                    endIndent: 20,
-                  ),
-                  const SizedBox(height: MARGIN),
-
-                  for (var i = 0; i < 5; i++) _comment("body $i", context),
-
-                  const TextField(
-                    decoration: InputDecoration(
-                      border: UnderlineInputBorder(),
-                      hintText: 'Comment',
-                    ),
-                    textInputAction: TextInputAction.send,
-                  ),
-                  const SizedBox(height: MARGIN),
-                ],
-              ),
+            Row(
+              children: [
+                const Icon(Icons.location_on),
+                const SizedBox(width: MARGIN),
+                Column(
+                  children: [
+                    Text('address', style: Theme.of(context).textTheme.headline6),
+                    const Text('coords'),
+                  ],
+                ),
+              ],
             ),
+            const SizedBox(height: MARGIN),
+
+            Row(
+              children: [
+                const Icon(Icons.access_time),
+                const SizedBox(width: MARGIN),
+                Text('time', style: Theme.of(context).textTheme.headline6),
+              ],
+            ),
+            const SizedBox(height: MARGIN),
+
+            const PhotoGallery(false),
+            const SizedBox(height: MARGIN),
+
+            Row(
+              children: const [
+                Icon(Icons.description),
+                SizedBox(width: MARGIN),
+                Text('description'),
+              ],
+            ),
+            const SizedBox(height: MARGIN),
+
+            _comment("initial comment", context, username: "report sender"),
+
+            const Divider(
+              thickness: 5,
+              indent: 20,
+              endIndent: 20,
+            ),
+            const SizedBox(height: MARGIN),
+
+            for (var i = 0; i < 5; i++) _comment("body $i", context),
+
+            const TextField(
+              decoration: InputDecoration(
+                border: UnderlineInputBorder(),
+                hintText: 'Comment',
+              ),
+              textInputAction: TextInputAction.send,
+            ),
+            const SizedBox(height: MARGIN),
           ],
         );
       },
