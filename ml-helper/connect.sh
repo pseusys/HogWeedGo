5511e987e1add615a181a98a04bef19a22015218
milesartemius <shveitsar215@gmail.com>
2022-01-18 07:14:57 +0300

    ml part initialized

--- ml-helper/connect.sh
+++
@@ -1,6 +0,0 @@
-pip install jupyter_http_over_ws
-jupyter serverextension enable --py jupyter_http_over_ws
-jupyter notebook \
-  --NotebookApp.allow_origin='https://colab.research.google.com' \
-  --port=8888 \
-  --NotebookApp.port_retries=0
