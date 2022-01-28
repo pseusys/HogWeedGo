8fbde5497869d5a58d38045888419bb681842d37
milesartemius <shveitsar215@gmail.com>
2022-01-28 10:00:26 +0300

    model after-published

--- ml-helper/test.py
+++ ml-helper/test.py
@@ -1,11 +1,13 @@
 from tensorflow.python.framework.config import set_visible_devices, get_visible_devices
-from tensorflow.python.keras.backend import softmax, expand_dims
 from tensorflow.lite.python.interpreter import Interpreter
+from tensorflow.keras.backend import softmax, expand_dims
+from argparse import ArgumentParser
 from numpy import asarray, argmax
 from pandas import read_csv
 from requests import get
 from io import BytesIO
 from PIL import Image
+from sys import exit
 
 
 def predict(inter, image):
@@ -26,8 +28,14 @@ try:
 except:
     print("Failed to disable GPUs, testing may be inaccurate")
 
+# Prepare argument parser
+parser = ArgumentParser(description="Test created detector '.tflite' neural network")
+parser.add_argument('-n', '--network', required=True, help="Network, saved in '.tflite' file")
+parser.add_argument('-s', '--source', required=True, help="Dataset containing test dataset of labels and photo URLs")
+args = vars(parser.parse_args())
+
 # Load TFLite model and allocate tensors.
-interpreter = Interpreter(model_path="models/detector-0.95487.tflite")
+interpreter = Interpreter(model_path=args['network'])
 interpreter.allocate_tensors()
 
 # Get input and output tensors.
@@ -36,7 +44,7 @@ output_details = interpreter.get_output_details()
 
 # Test model on prepared test input data.
 matches = []
-test_data = read_csv("./datasets/validation.csv").to_records(index=False)
+test_data = read_csv(args['source']).to_records(index=False)
 for label, URL in test_data:
     try:
         img_data = Image.open(BytesIO(get(URL).content))
@@ -49,3 +57,4 @@ for label, URL in test_data:
 # Print testing result.
 test_prob = sum(matches) / len(matches)
 print(f"Test probability of model is: {round(test_prob, 3)} {'❌' if test_prob < DESIRED_RESULT else '✅'}")
+exit(1 if test_prob < DESIRED_RESULT else 0)
