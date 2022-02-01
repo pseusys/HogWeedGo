6fa9104fd1bbfa8a0311b2d80c3d0578a37eed62
milesartemius <shveitsar215@gmail.com>
2022-02-02 00:23:04 +0300

    docker-compose configs final

--- server/docker/generate-server-release.sh
+++ server/docker/generate-server-release.sh
@@ -2,7 +2,7 @@
 mkdir bundled-server
 
 # https://github.com/docker/compose/issues/7964
-# docker-compose -f docker/docker-compose.yml -f docker/docker-compose.production.yml --env-file=docker/.env.development config | sed 's/\.\./\./' > bundled-server/docker-compose.bundle.yml
+# docker-compose -f docker/docker-compose.yml -f docker/docker-compose.production.yml --env-file=docker/.env.development config --no-interpolate | sed 's/\.\./\./' > bundled-server/docker-compose.bundle.yml
 cp docker/docker-compose.bundle.yml bundled-server
 
 cp samples/sample_config.env bundled-server
