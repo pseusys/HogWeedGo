26a35161fcc312f5080e441d0ee786fc3643d603
milesartemius <shveitsar215@gmail.com>
2022-04-18 08:37:57 +0300

    actions released

--- server/README.md
+++ server/README.md
@@ -10,7 +10,15 @@
  - Debug mode: `docker-compose -f ./docker/docker-compose.yml --env-file=./docker/.env.development up`
  - Release mode: `docker-compose -f ./docker/docker-compose.yml --env-file=./docker/.env.production up`
  
-### Image will be published using GitHub
+### Launch server locally
+1. Download `bundled-server.zip` artifact from releases section, open it and unpack, run shell console there
+2. Run script `./config-generator [DOMAIN]`, where DOMAIN - is your server domain name
+3. Run `docker-compose -f ./docker-compose.yml --env-file=./system-config.env up`
+4. Enjoy your server!  
+NB! All the generated `*config.env` files are safe to edit BEFORE the first server launch. After that you shouldn't be modified in any way.
+NB! By default, server runs on HTTPS, redirecting all HTTP traffic there. Ports for HTTP and HTTPS can be found in `./system-config.env up`, make sure they are free and available to use!  
+NB! HTTPS certificates stored in `./certificates` folder are auto-generated and self-signed. Feel free to replace them with trusted ones.  
+NB! Superuser email and password for accessing admin website are stored in `./config.env` file (DJANGO_SUPERUSER_EMAIL and DJANGO_SUPERUSER_PASSWORD respectively).
 
 HogWeed Server roadmap:
 - [x] Server website
--- server/init-local.sh
+++ server/init-local.sh
@@ -44,14 +44,14 @@ if [ $# -eq 1 ]; then
   [ "$(python3 -c 'import sys; print(".".join(map(str, sys.version_info[:2])))')" == '3.10' ] || print_error "Python 3.10 appears not to be installed, visit following link for installation guide: https://www.python.org/downloads/release/python-3100"
   psql --version > /dev/null || print_error "PostgreSQL appears not to be installed run following command to fix this: 'apt install postgresql-13 postgresql-client-13 postgresql-contrib postgis postgresql-13-postgis-3 gdal-bin"
   echo "Check PostgreSQL availability for user $POSTGRES_ADMIN"
-  sudo -u "$POSTGRES_ADMIN" psql -c "" || print_error "Current user is not an administrator for PostgreSQL or has a password. Remove admin password (if any) and run the script again with 'sudo -u admin ...'"
+  psql -U "$POSTGRES_ADMIN" -c "" || print_error "Current user is not an administrator for PostgreSQL or has a password. Remove admin password (if any) and run the script again with 'sudo -u admin ...'"
 
   echo "Create PostgreSQL user '$POSTGRES_USER'"
-  sudo -u "$POSTGRES_ADMIN" psql -c "CREATE USER $POSTGRES_USER WITH PASSWORD '$POSTGRES_PASSWORD';"
+  psql -U "$POSTGRES_ADMIN" -c "CREATE USER $POSTGRES_USER WITH PASSWORD '$POSTGRES_PASSWORD';"
   echo "Create PostgreSQL database '$POSTGRES_DB'"
-  sudo -u "$POSTGRES_ADMIN" psql -c "CREATE DATABASE $POSTGRES_DB WITH OWNER $POSTGRES_USER ENCODING UTF8;"
+  psql -U "$POSTGRES_ADMIN" -c "CREATE DATABASE $POSTGRES_DB WITH OWNER $POSTGRES_USER ENCODING UTF8;"
   echo "Create PostGIS extension for database '$POSTGRES_DB' if not exists"
-  sudo -u "$POSTGRES_ADMIN" psql -d "$POSTGRES_DB" -c "CREATE EXTENSION IF NOT EXISTS postgis;" &&
+  psql -U "$POSTGRES_ADMIN" -d "$POSTGRES_DB" -c "CREATE EXTENSION IF NOT EXISTS postgis;" &&
   echo "Restart PostgreSQL"
   service postgresql restart
 
