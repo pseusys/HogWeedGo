276b581c18c8b3e1fbf82294032de9e4e0db341c
milesartemius <shveitsar215@gmail.com>
2022-05-26 05:16:22 +0300

    model test updated - time added

--- ml-helper/test.py
+++ ml-helper/test.py
@@ -6,6 +6,7 @@ from numpy import asarray, argmax
 from pandas import read_csv
 from requests import get
 from io import BytesIO
+from time import time
 from PIL import Image
 from sys import exit
 
@@ -44,17 +45,21 @@ output_details = interpreter.get_output_details()
 
 # Test model on prepared test input data.
 matches = []
+time_total = 0
 test_data = read_csv(args['source']).to_records(index=False)
 for label, URL in test_data:
     try:
         img_data = Image.open(BytesIO(get(URL).content))
         if len(img_data.getdata()) > 0:
+            lap_time = time()
             result = predict(interpreter, img_data.resize(input_details[0]['shape'][1:3]))
+            time_total += time() - lap_time
             matches.append(1 if CLASS_NAMES.index(label) == argmax(result) else 0)
     except Exception as e:
         print(f"Test link {URL} invalid, skipping:\n{e}")
 
 # Print testing result.
 test_prob = sum(matches) / len(matches)
+print(f"Model average execution time is: {round(time_total / len(matches), 1)} sec")
 print(f"Test probability of model is: {round(test_prob, 3)} {'❌' if test_prob < DESIRED_RESULT else '✅'}")
 exit(1 if test_prob < DESIRED_RESULT else 0)
